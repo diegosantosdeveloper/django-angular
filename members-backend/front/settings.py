59fe146f81500f35a90ee25ef6bbd9d68eea5989
Diego Patrick da Silva Santos <diegosantos.developer@gmail.com>
2022-09-15 19:00:29 -0300

    Adicionando configuração para arquivo estáticos

--- members-backend/front/settings.py
+++ members-backend/front/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import pathlib
 
@@ -129,9 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
