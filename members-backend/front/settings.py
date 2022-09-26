b28c7d8ea62310fbe7928f9f1863aa65d45839da
Diego Patrick da Silva Santos <diegosantos.developer@gmail.com>
2022-09-26 19:13:03 -0300

    Configurações atualizadas

--- members-backend/front/settings.py
+++ members-backend/front/settings.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
-import os
 from pathlib import Path
-import django_heroku
-import pathlib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +24,7 @@ SECRET_KEY = 'django-insecure-iy&8n8!ie(@#*#zdus*wyhfuz^gest@lc5u!yf=-ta$1u1jks(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://gestao-members.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,8 +131,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -146,5 +142,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200'
 ]
-
-django_heroku.settings(locals())
